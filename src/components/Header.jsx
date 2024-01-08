2aa0f6183e360f86f274e9c503923b977aee4893
kevinstaresdarbon <red.inshou@gmail.com>
2024-01-08 12:46:09 +0000

    Adds Header to first page

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+
+function Header() {
+    return (
+        <header class="row" id="header" style="max-height: 10rem">
+            <div class="col-12 p-0">
+                {/* <!-- card copied and modified from getbootstrap.com card element --> */}
+                <div class="card rounded-0 border-0 h-100 w-100" style="background-color: #800D98;">
+                    <div class="card-body d-flex w-100 h-100">
+                        <p class="card-title mb-0 me-2 p-3 fs-5 align-self-start nav-card-style fw-bold" style="width: 9rem;">
+                            Freelance Web Developer</p>
+                        <Navbar />
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import personnelLogo from '/favicon.png';
+
+function Navbar(){
+    return (
+        <nav class="navbar navbar-expand-md ms-auto nav-card-style" style="z-index: 6;">
+            <div class="container-fluid">
+                <a class="navbar-brand me-1" href="#"><img src={personnelLogo} alt="purple personnel icon" width="32" height="32" /> </a>
+                <button class="navbar-toggler ms-1" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link fs-custom" aria-current="page" href="#about">About</a>
+                        <a class="nav-link fs-custom" href="#skills">Skills</a>
+                        <a class="nav-link fs-custom" href="#work">Work</a>
+                        <a class="nav-link fs-custom" href="#contact">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
