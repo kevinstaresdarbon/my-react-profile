e2d2da5ae0018a35a9cf873af6d4ca90a414e5f3
kevinstaresdarbon <red.inshou@gmail.com>
2024-06-26 05:48:47 +0100

    adds updates to the about blurb

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,10 +14,11 @@ function About() {
                         <p className="fs-4 fw-bold px-2"> Welcome to my Online Profile!</p>
                         <p className="fs-4 fw-bold px-2"> I am an enthusiastic hobbyist who is turning his passion into his career.
                         </p>
-                        <p className="fs-4 fw-bold px-2">IT has been a hobby of mine for over twenty years in which time I have set-up varying game Servers from TrinityCore to Minecraft, VPS provisioning servers, web pages, wordpress sites and much more!.</p>
-                        <p className="fs-4 fw-bold px-2">In the last few years web design and web development have caught my interest.</p>
+                        <p className="fs-4 fw-bold px-2">IT has been a hobby of mine for over twenty years in which time I have set-up varying game Servers from TrinityCore to Minecraft, VPS provisioning servers, web pages, wordpress sites and much more!</p>
+                        <p className="fs-4 fw-bold px-2">In the last few years web design and web development have caught my interest and as a lifelong learner I have been continually expanding my knowledge and skillsets.</p>
                         <p className="fs-4 fw-bold px-2">I have taken web development bootcamps on udemy and also studied some of their react, vue and express courses.</p>
-                        <p className="fs-4 fw-bold px-2"> I am currently taking the "edX: Front-End Web Development Bootcamp".</p>
+                        <p className="fs-4 fw-bold px-2"> I recently passed the "edX: Front-End Web Development Bootcamp" and am currently taking "The Skills Network: Service Desk Technician Bootcamp" both of which are funded by the U.K. government</p>
+                        <p className="fs-4 fw-bold px-2">I am interested in gaining more private work or a full-time role in an I.T. company.  I am highly educated with a large variety of I.T. skills and will be an asset to anyone who employs my services</p>
                     </div>
                 </div>
             </div>
